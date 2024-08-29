ad20f4b834971bcf9061841dbdbcfe90de440da5
Zerin adiba <167603609+zerinadiba@users.noreply.github.com>
2024-08-29 20:01:48 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,265 @@
+//example1
+function textChange(){
+	document.getElementById('tc').innerHTML="welcome to my world";
+}
+//example2
+function showDate(){
+	document.getElementById('sd').innerHTML=Date();
+}
+
+//example3
+function bulbOn(){
+	document.getElementById('bulb').src='images/on.gif';
+}
+function bulbOff(){
+	document.getElementById('bulb').src='images/off.gif';
+}
+//example4
+function changeCss(){
+	document.getElementById('css').style.color="purple";
+}
+
+//example5
+function textShow(){
+	document.getElementById('csh').style.display="block";
+}
+function textHide(){
+	document.getElementById('csh').style.display="none";
+}
+
+//example6
+function changeFont_size(){
+	document.getElementById('fc').style.fontSize="50px";
+}
+
+//example7
+
+ function smile(){
+	document.getElementById('emoji').src='images/smile.jpg';
+}
+function sad(){
+	document.getElementById('emoji').src='images/sad.jpg';
+}
+
+//example8
+function changeBgc(){
+	document.getElementById('fbcg').style.backgroundColor="teal";
+}
+
+//example9
+
+function picShow(){
+	document.getElementById('sc').style.display= 'block';
+}
+
+function picHide(){
+	
+	document.getElementById('sc').style.display= 'none';
+}
+
+
+
+//example10
+function show(){
+	document.getElementById('scbs').style.backgroundColor='red';
+}
+
+function hide(){
+	
+	document.getElementById('scbs').style.backgroundColor= 'transparent';
+}
+//example11
+function changeBorder(){
+	document.getElementById('cb').style.border= '5px solid black';
+}
+//example12
+function bShow(){
+	document.getElementById('bb').style.border='5px dashed red';
+}
+
+function bHide(){
+	
+	document.getElementById('bb').style.border= 'none';
+}
+//example13
+function alignItem(){
+	document.getElementById('ai').style.textAlign= 'center';
+}
+//example14
+function bcgImg(){
+	document.getElementById('bimg').style.backgroundImage= "url('images/d3.png')";
+}
+//example15
+function ca(){
+	document.getElementById("myDIV").style.animation = "mynewmove 4s 2";
+}
+
+//example16
+function cda(){
+	document.getElementById("mDiv").style.animationDelay = "3s";
+}
+
+//example17
+function bcgClip(){
+	document.getElementById("bclip").style.backgroundClip = "content-box";
+}
+
+
+//example18
+function bcgPos(){
+	document.getElementById("bmg").style.backgroundPosition="top right"; 
+}
+//example19
+function bcgSize(){
+	document.getElementById("bs").style.backgroundSize = "60px 120px";
+}
+
+//example20
+
+function myFunction(){
+	document.getElementById("bf").style.backfaceVisibility = "visible";
+}
+function myFunction(){
+	document.getElementById("bf").style.backfaceVisibility = "hidden";
+}
+
+//example21
+function changeBot(){
+	document.getElementById('cbb').style.borderBottom = '2px  solid red';
+}
+//example22
+function changeBor(){
+	document.getElementById('cbbr').style.borderBottomLeftRadius = "25px";
+}
+//example23
+function changeBorr(){
+	document.getElementById('cbbrr').style.borderBottomRightRadius = "25px";
+}
+
+//example24
+function changeBots(){
+	document.getElementById('cbbs').style.borderBottom = '2px  dashed red';
+}
+
+//example25
+function changeBotw(){
+	document.getElementById('cbbw').style.borderBottomWidth = "10px";
+}
+//example26
+function changeBorl(){
+	document.getElementById('cbbl').style.borderLeft = " 5px solid";
+}
+//example27
+function changeBorls(){
+	document.getElementById('cbbls').style.borderLeftStyle = " dashed";
+}
+//example28
+function changeBorr(){
+	document.getElementById('cbbrrr').style.borderRight = "thick solid";
+}
+//example29
+function changeBorrrs(){
+	document.getElementById('cbbrrrs').style.borderRightStyle = " dashed";
+}
+//example30
+function changeBoxs(){
+	document.getElementById('cboxs').style.boxShadow = "10px 20px 30px red";
+}
+
+
+//example31
+function myFunction() {
+  document.getElementById("demo").style.direction = "rtl";
+}
+
+//example32
+function myFunction() {
+  document.getElementById("dem").style.display = "none";
+}
+//example33
+function myFunction() {
+  document.getElementById("ff").style.fontFamily = "Volkhov, serif";
+}
+//example34
+function myFunctions() {
+  document.getElementById("fs").style.fontStyle = "italic";
+}
+//example35
+function myFunctionv() {
+  document.getElementById("fv").style.fontVariant = "small-caps";
+}
+
+//example36
+function myFunctionw() {
+  document.getElementById("fw").style.fontWeight = "900";
+}
+
+//example37
+function myFunctionb() {
+  document.getElementById("myBtn").style.height = "50px";
+}
+//example38
+
+function myFunctionbx() {
+  document.getElementById("box").style.justifyContent = "space-between";
+}
+//example39
+function myFunctionl() {
+  document.getElementById("myList").style.listStyle = "decimal inside";
+}
+//example40
+
+function myFunctionlp() {
+  document.getElementById("myUL").style.listStylePosition = "inside";
+}
+
+//example41
+
+function myFunctionpr() {
+  document.getElementById("myULI").style.listStyleType = "upper-roman";
+}
+
+//example42
+function myFunctionm() {
+  document.getElementById("mr").style.margin = "50px 10px 20px 30px";
+}
+
+//example43
+function myFunctionmb() {
+  document.getElementById("mb").style.marginBottom = "50px";
+}
+
+//example44
+function myFunctionml() {
+  document.getElementById("ml").style.marginLeft = "50px";
+}
+
+
+//example45
+function myFunctionmrr() {
+  document.getElementById("mrr").style.marginRight = "50px";
+}
+//example46
+function myFunctionmt() {
+  document.getElementById("mt").style.marginTop = "50px";
+}
+
+//example47
+function myFunctionmh() {
+  document.getElementById("mh").style.maxHeight = "100px";
+}
+
+//example48
+function myFunctiontd() {
+  document.getElementById("td").style.textDecoration = "underline overline";
+}
+
+//example49
+function myFunctiontdc() {
+  document.getElementById("tdc").style.textDecorationColor = "red";
+}
+
+//example50
+function myFunctiontt() {
+  document.getElementById("tt").style.textTransform = "capitalize";
+}
